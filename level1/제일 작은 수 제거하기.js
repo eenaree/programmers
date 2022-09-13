8f3640a432b83596f77a0f255005a2fef46d0d90
eenaree <narilee91@gmail.com>
2022-09-13 17:47:19 +0900

    [level1] 제일 작은 수 제거하기 - add solution2
    배열값이 1개만 있는 경우, 불필요하게 filter 메서드 실행하지 않도록 변경

--- level1/제일 작은 수 제거하기.js
+++ level1/제일 작은 수 제거하기.js
@@ -16,3 +16,14 @@ console.log(solution([4, 3, 2, 1]));
 console.log(solution([1, 2, 3, 4]));
 console.log(solution([-2, -4, -6, 1, 3]));
 console.log(solution([10]));
+
+// 20220913 - 2
+// 배열의 길이가 1이라면 => -1 리턴
+// 아니라면 => 최소값 구하고, 배열에서 최소값 제거 => 배열 리턴
+
+function solution2(arr) {
+  if (arr.length == 1) return [-1];
+
+  const min = Math.min(...arr);
+  return arr.filter(v => v !== min);
+}
